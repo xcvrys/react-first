8dfd1a6f92bbda796ed59990e3560cf18416e4ab
xcvrys <106309479+xcvrys@users.noreply.github.com>
2022-10-03 20:17:09 +0200

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className="footer">
-      <Link to="/">
+      <Link to="/react-first">
         <a href='#'>BACK</a>
       </Link>
     </div>
